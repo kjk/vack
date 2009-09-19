603764a103b2184251c9298dea621015e6d9e2ca
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-09-18 18:46:07 -0700

    start on VisualAck

---
+++ VisualAck/VisualAckAppDelegate.h
@@ -0,0 +1,10 @@
+#import <Cocoa/Cocoa.h>
+#import <AppKit/NSApplication.h>
+
+@interface VisualAckAppDelegate : NSObject {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
