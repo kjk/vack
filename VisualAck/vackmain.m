47cb522bb8cfc56564fe9ac6cf3b970e57f73ae7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-09-23 19:08:08 -0700

    started on command-line app

---
+++ VisualAck/vackmain.m
@@ -0,0 +1,8 @@
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    printf("Vack attack!\n");
+    [pool release];
+}
