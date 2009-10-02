4c3a92ac855532ecfb63a550ab4d5184b00d8db0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-10-02 00:32:54 -0700

    get file attributes when searching and distinguish between files and directories

--- VisualAck/FileSearcher.m
+++ VisualAck/FileSearcher.m
@@ -1,5 +1,9 @@
 #import "FileSearcher.h"
 
+@interface FileSearcher(Private)
+- (BOOL)shouldSkipDirectory:(NSString*)directory;
+@end
+
 @implementation FileSearcher
 
 - (id)initWithDirectory:(NSString*)path {
@@ -17,12 +21,28 @@
     return delegate_;
 }
 
+- (BOOL)shouldSkipDirectory:(NSString*)directory {
+    // TODO: write me. Skip .git, .svn etc.
+    return NO;
+}
+
 - (void)startSearch {
     NSLog(@"startSearch");
     NSDirectoryEnumerator *dirEnum = [[NSFileManager defaultManager] enumeratorAtPath:startDir_];
     NSString *file;
     for (file in dirEnum) {
-	NSLog(@"file: %@", file);
+	NSDictionary *fileAttrs = [dirEnum fileAttributes];
+	NSString* fileType = [fileAttrs valueForKey:NSFileType];
+	if ([fileType isEqualToString:NSFileTypeRegular]) {
+	    NSLog(@"file     : %@", file);
+	} else if ([fileType isEqualToString:NSFileTypeDirectory]) {
+	    NSLog(@"directory: %@", file);
+	    if ([self shouldSkipDirectory:file]) {
+		[dirEnum skipDescendents];
+	    }
+	} else {
+	    NSLog(@"unhandled type %@ for file %@", fileType, file);
+	}
     }
     NSLog(@"endSearch");
 }
