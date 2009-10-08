4bdb87c91ff73b8c11a1dbf3a3fe3269a9bce580
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-10-07 22:31:07 -0700

    start on [FileLineIterator getNextLine]

--- VisualAck/FileLineIterator.h
+++ VisualAck/FileLineIterator.h
@@ -1,10 +1,14 @@
 #import <Cocoa/Cocoa.h>
 
+#define FILE_BUF_SIZE 8*1024
 
 @interface FileLineIterator : NSObject {
     NSString *	    path_;
     FILE *	    fp_;
     int		    currLineNo_;
+    char	    buf_[FILE_BUF_SIZE];
+    int		    posInBuf_;
+    int		    charsInBuf_;
 }
 
 - (id)initWithFileName:(NSString*)path;
--- VisualAck/FileLineIterator.m
+++ VisualAck/FileLineIterator.m
@@ -1,4 +1,5 @@
 #import "FileLineIterator.h"
+#import <assert.h>
 
 @implementation FileLineIterator
 
@@ -12,8 +13,14 @@
 	fclose(fp_);
 }
 
+- (int) leftInBuf {
+    assert(charsInBuf_ >= posInBuf_);
+    return charsInBuf_ - posInBuf_;
+}
+
 // Return next line from the file, nil if end of file. <lineNo> is the line number.
 - (NSString*)getNextLine:(int*)lineNo {
+    NSString *s = nil;
     if (!fp_) {
 	const char *filepath = [path_ UTF8String];
 	fp_ = fopen(filepath, "r");
@@ -21,12 +28,47 @@
 	if (!fp_)
 	    return nil;
 	currLineNo_ = 0;
+	charsInBuf_ = 0;
+	posInBuf_ = 0;
     }
 
+    assert(fp_);
     // TODO: write me
+    if (0 == [self leftInBuf]) {
+	size_t readBytes = fread((void*)buf_, 1, FILE_BUF_SIZE , fp_);
+	if (0 == readBytes)
+	    return nil;
+	charsInBuf_ = readBytes;
+	posInBuf_ = 0;
+    }
+
+    int left = [self leftInBuf];
+    assert(left > 0);
+    char *start = &(buf_[posInBuf_]);
+    char *curr = start;
+    while (left > 0) {
+	char c = *curr;
+	if (c == '\n' || c == '\r') {
+	    break;
+	}
+	++curr;
+	left--;
+    }
+
+    // TODO: update posInBuf_
+    // TODO; handle \n\r so that it doesn't show as an empty line
+    // didn't find newline
+    // TODO: handle line spanning buffers
+    if (0 == left) {
+	goto Exit;
+    }
+    int len = curr - start;
+    s = [NSString stringWithCString:start length:len];
     *lineNo = currLineNo_;
+
+Exit:
     currLineNo_++;
-    return nil;
+    return s;
 }
 
 @end
