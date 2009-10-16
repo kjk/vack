afdb7abad22e260a4ecc53926c66a1d99334eadc
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-10-16 01:12:05 -0700

    start on unit tests

---
+++ VisualAck/utvackmain.m
@@ -0,0 +1,18 @@
+#import <Cocoa/Cocoa.h>
+
+//#import "FileSearchProtocol.h"
+//#import "FileSearcher.h"
+
+#import "FileLineIterator.h"
+
+void testFileLineIterator() {
+    
+}
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    [pool drain];
+    return 0;
+}
+
