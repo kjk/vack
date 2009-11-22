87edca7a28b6432ac0bc1bc767e8a8be240b99b6
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2009-11-22 01:47:58 -0800

    start on a build script

---
+++ VisualAck/build.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+# What: build VisualAck and (optionally) upload it to s3
+# How:
+#   * extract program version from .plist file
+#   * build
+#   * upload to s3 kjkpub bucket. Uploaded files:
+#       vack/SumatraPDF-prerelase-<svnrev>.exe
+#       vack/sumatralatest.js
+#       vack/sumpdf-prerelease-latest.txt
+
+import sys, os, os.path, subprocess, time
+try:
+    import boto.s3
+    from boto.s3.key import Key
+except:
+    print("You need boto library (http://code.google.com/p/boto/)")
+    print("svn checkout http://boto.googlecode.com/svn/trunk/ boto")
+    print("cd boto; python setup.py install")
+    raise
+
+try:
+    import awscreds
+except:
+    print "awscreds.py file needed with access and secret globals for aws access"
+    sys.exit(1)
+
+SRC_DIR = os.path.dirname(os.path.realpath(__file__))
+RELEASE_BUILD_DIR = os.path.join(SRC_DIR, "build", "Release")
+INFO_PLIST_PATH = os.path.realpath(os.path.join(SRC_DIR, "VisualAck-Info.plist"))
+APP_CAST_PATH = os.path.join(SRC_DIR, "appcast_template.xml")
+S3_APPCAST_NAME = "bterm/appcast.xml"
+S3_LATEST_VER_NAME = "bterm/latestver.js"
+S3_RELNOTES_PATH = "bterm/relnotes.html"
+S3_ATOM_PATH = "bterm/relnotes.xml"
+
+S3_BUCKET = "kjkpub"
+g_s3conn = None
+
+def s3connection():
+  global g_s3conn
+  if g_s3conn is None:
+    g_s3conn = boto.s3.connection.S3Connection(awscreds.access, awscreds.secret, True)
+  return g_s3conn
+
+def s3PubBucket(): return s3connection().get_bucket(S3_BUCKET)
+
+def ul_cb(sofar, total):
+  print("So far: %d, total: %d" % (sofar , total))
+
+def s3UploadFilePublic(local_file_name, remote_file_name):
+  bucket = s3PubBucket()
+  k = Key(bucket)
+  k.key = remote_file_name
+  k.set_contents_from_filename(local_file_name, cb=ul_cb)
+  k.make_public()
+
+def s3UploadDataPublic(data, remote_file_name):
+  bucket = s3PubBucket()
+  k = Key(bucket)
+  k.key = remote_file_name
+  k.set_contents_from_string(data)
+  k.make_public()
+
+# like cmdrun() but throws an exception on failure
+def run_cmd_throw(*args):
+  cmd = " ".join(args)
+  print("\nrun_cmd_throw: '%s'" % cmd)
+  cmdproc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  res = cmdproc.communicate()
+  errcode = cmdproc.returncode
+  if 0 != errcode:
+    print("Failed with error code %d" % errcode)
+    print("Stdout:")
+    print(res[0])
+    print("Stderr:")
+    print(res[1])
+    raise Exception("'%s' failed with error code %d" % (cmd, errcode))
+  return (res[0], res[1])
+
+def exit_with_error(s):
+    print(s)
+    sys.exit(1)
+
+def ensure_file_exists(path):
+    if not os.path.exists(path) or not os.path.isfile(path):
+        exit_with_error("File '%s' desn't exist" % path)
+
+# build version is either x.y or x.y.z
+def ensure_valid_version(version):
+    m = re.match("\d+\.\d+", version)
+    if m: return
+    m = re.match("\d+\.\d+\.\d+", version)
+    if m: return
+    print("version ('%s') should be in format: x.y or x.y.z" % version)
+    sys.exit(1)
+
+# a really ugly way to extract version from Info.plist
+def extract_version_from_plist(plist_path):
+    plist = readfile(plist_path)
+    #print(plist)
+    regex = re.compile("CFBundleVersion</key>(.+?)<key>", re.DOTALL | re.MULTILINE)
+    m = regex.search(plist)
+    version_element = m.group(1)
+    #print("version_element: '%s'" % version_element)
+    regex2 = re.compile("<string>(.+?)</string>")
+    m = regex2.search(version_element)
+    version = m.group(1)
+    version = version.strip()
+    #print("version: '%s'" % version)
+    return version
+
+def main():
+    ensure_file_exists(INFO_PLIST_PATH)
+    ensure_file_exists(APP_CAST_PATH)
+    version = extract_version_from_plist(INFO_PLIST_PATH)
+    print(version)
+
+if __name__ == "__main__":
+    main()
